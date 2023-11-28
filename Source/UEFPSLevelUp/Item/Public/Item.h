1998b52f788808de1a01f9f942e70e0fa04e1ba8
Soraku7 <2673140287@qq.com>
2023-11-28 20:32:07 +0800

    Item Rarity

--- Source/UEFPSLevelUp/Item/Public/Item.h
+++ Source/UEFPSLevelUp/Item/Public/Item.h
@@ -10,6 +10,19 @@ class USphereComponent;
 class UWidgetComponent;
 class UBoxComponent;
 
+UENUM(BlueprintType)
+enum class EItemRarity : uint8
+{
+	EIR_Damaged UMETA(DisplayName = "Damaged"),
+	EIR_Common UMETA(DisplayName = "Common"),
+	EIR_UnCommon UMETA(DisplayName = "UnCommon"),
+	EIR_Rare UMETA(DisplayName = "Rare"),
+	EIR_Legendary UMETA(DisplayName = "Legendary"),
+
+	EIR_MAX UMETA(Display = "DefaultMAX")
+
+};
+
 UCLASS()
 class UEFPSLEVELUP_API AItem : public AActor
 {
@@ -37,6 +50,11 @@ protected:
 		UPrimitiveComponent* OtherComp,
 		int32 OtherBodyIndex);
 
+	
+	/**
+	 * 设置物品稀有度
+	 */
+	void SetActiveStars();
 public:
 	// Called every frame
 	virtual void Tick(float DeltaTime) override;
@@ -72,6 +90,17 @@ private:
 	 */
 	UPROPERTY(EditAnywhere , BlueprintReadWrite , Category = "Item Propertier" , meta = (AllowPrivateAccess = "true"))
 	int32 ItemCount;
+
+	
+	/**
+	 * 物品稀有度
+	 */
+	UPROPERTY(EditAnywhere , BlueprintReadOnly , Category = "Item Propertier" , meta = (AllowPrivateAccess = "true"))
+	EItemRarity ItemRarity;
+
+	UPROPERTY(VisibleAnywhere , BlueprintReadOnly , Category = "Item Propertier" , meta = (AllowPrivateAccess = "true"))
+	TArray<bool> ActiveStars;
+	
 public:
 	FORCEINLINE UWidgetComponent* GetPickupWidget() const {return PickupWidget;};
 
